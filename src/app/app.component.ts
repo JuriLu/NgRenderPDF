ecd4a8ed88e1239734ac1b399c8b637c179d2a82
Juri <lubonjajurgen1@gmail.com>
2023-05-22 15:20:07 +0200

    Initial commit + Configurations + install grapecity + Render pdf reader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import {GcPdfViewer} from "@grapecity/gcpdfviewer";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<div id="viewer"></div>',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'PDFViewer';
+
+  ngAfterViewInit(){
+    const viewer = new GcPdfViewer("#viewer", {
+      workerSrc: "//node_modules/@grapecity/gcpdfviewer/gcpdfviewer.worker.js",
+      restoreViewStateOnLoad: false
+    });
+    viewer.addDefaultPanels();
+    viewer.open('/assets/impatient-js-preview-book.pdf')
+  }
 }
